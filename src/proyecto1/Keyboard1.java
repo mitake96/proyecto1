9e566cab78981208ad0e7c3452eaaf9df2ed2af6
Unknown <nacho_085@hotmail.com>
2018-11-25 17:48:57 +0100

    Cerrar teclado
    Ejercicio empezado + teclado cerrado

---
+++ src/proyecto1/Keyboard1.java
@@ -0,0 +1,19 @@
+package proyecto1;
+
+import java.util.Scanner;
+
+public class Keyboard1 {
+
+	static Scanner keyboard = new Scanner(System.in);
+	
+	public static void closeKeyboard() {
+		keyboard.close();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		
+	}
+
+}
